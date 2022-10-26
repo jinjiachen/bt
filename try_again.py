764cc8e9d582fc83084f89536731e8b5f46bfbc2
jinjiachen <jinjiachen_tux@163.com>
2022-10-26 23:42:46 +0800

    买卖不及预期，待调试'

--- try_again.py
+++ try_again.py
@@ -9,9 +9,10 @@ import numpy as np
 import os.path  # To manage paths
 import sys  # To find out the script name (in argv[0])
 #from sqlalchemy import create_engine 
-
 # Import the backtrader platform
 import backtrader as bt
+#from matplotlib import plot
+import pdb
 
 #Create a strategy
 class TestStrategy(bt.Strategy):
@@ -28,13 +29,19 @@ class TestStrategy(bt.Strategy):
         #Keep a reference to the "close" line in the data[0] dataseries
         self.count=0
         self.dataclose = self.datas[0].close
-        print('close[0]:',self.dataclose[0])
+#        print('close[0]:',self.dataclose[0])
+#        self.log('Close, %2f' % self.dataclose[0])
         self.dataopen = self.datas[0].open
         self.datetime = self.datas[0].datetime
-        self.log('Datetime, %2f' % self.datetime[0])
-#        print(len(self.datas[0].open))
+        self.sma=bt.indicators.SimpleMovingAverage(self.datas[0],period=21)
+#        self.log('Datetime, %2f' % self.datetime[0])
+        print(self.datas[0])
+        print(self.datas[0].close)
+        print(self.datas[0].open)
+        print(len(self.datas[0].lines))
         # To keep track of pending orders
         self.order = None
+#        pdb.set_trace()
 
     def notify_order(self, order):
         if order.status in [order.Submitted, order.Accepted]:
@@ -45,9 +52,9 @@ class TestStrategy(bt.Strategy):
         # Attention: broker could reject order if not enough cash
         if order.status in [order.Completed]:
             if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
+                self.log('BUY EXECUTED, price:%.2f,cost:%.2f,comm:%.2f' %(order.executed.price,order.executed.value,order.executed.comm))
             elif order.issell():
-                self.log('SELL EXECUTED, %.2f' % order.executed.price)
+                self.log('SELL EXECUTED, price:%.2f,cost:%.2f,comm:%.2f' %(order.executed.price,order.executed.value,order.executed.comm))
 
             self.bar_executed = len(self)
 
@@ -57,39 +64,58 @@ class TestStrategy(bt.Strategy):
         # Write down: no pending order
         self.order = None
 
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
     def next(self):
         #Simply log the closing price of the series from the reference
         print(f'{self.count+1} loop of next')
         self.log('Close, %2f' % self.dataclose[0])
         self.log('Open, %2f' % self.dataopen[0])
         self.log('Datetime, %2f' % self.datetime[0])
-#        # Check if an order is pending ... if yes, we cannot send a 2nd one
-#        if self.order:
-#            return
-#
-#        # Check if we are in the market
-#        if not self.position:
-#
-#            # Not yet ... we MIGHT BUY if ...
-##            if self.dataclose[0] < self.dataclose[-1]:
+        self.log('Sma, %2f' % self.sma[0])
+        # Check if an order is pending ... if yes, we cannot send a 2nd one
+        if self.order:
+            return
+
+        # Check if we are in the market
+        if not self.position:
+
+            # Not yet ... we MIGHT BUY if ...
+#            if self.dataclose[0] < self.dataclose[-1]:
+#            if self.dataclose[0] > self.sma[0]:
 #            if True:
-#                    # current close less than previous close
-#
-#                # BUY, BUY, BUY!!! (with default parameters)
+#            if self.sma[0]>self.sma[-1]:
+            if self.sma[0]>self.sma[-1]:
+                if self.sma[-1]<self.sma[-2]:
+                    if self.sma[-2]<self.sma[-3]:
+                        self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                    # current close less than previous close
+
+                # BUY, BUY, BUY!!! (with default parameters)
 #                self.log('BUY CREATE, %.2f' % self.dataclose[0])
-#
-#                # Keep track of the created order to avoid a 2nd order
+
+                # Keep track of the created order to avoid a 2nd order
 #                self.order = self.buy(size=500)
-#
-#        else:
-#
-#            # Already in the market ... we might sell
+                self.order = self.buy()
+
+        else:
+
+            # Already in the market ... we might sell
 #            if len(self) >= (self.bar_executed + 5):
-#                # SELL, SELL, SELL!!! (with all possible default parameters)
-#                self.log('SELL CREATE, %.2f' % self.dataclose[0])
-#
-#                # Keep track of the created order to avoid a 2nd order
-#                self.order = self.sell()
+#            if self.sma[0]<self.sma[-1]:
+#            if self.dataclose[0]<self.sma[0]:
+            if self.dataclose[0]>self.sma[0]*1.2 or self.dataclose[0]<self.sma[0]:
+                # SELL, SELL, SELL!!! (with all possible default parameters)
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
         self.count+=1
         print('--'*20+'split')
 
@@ -102,22 +128,25 @@ if __name__ == '__main__':
 
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
-    datapath = './test.csv'
+#    datapath = './new.csv'
+    datapath=input('please input the data feed:')
+    datapath=datapath.replace('\'','')
 
     # Create a Data Feed
     data = bt.feeds.GenericCSVData(
+            saparator='\t',
             dataname=datapath,
         # Do not pass values before this date
-            fromdate=datetime.datetime(2020, 6, 1),
+            fromdate=datetime.datetime(2019, 1, 1),
         # Do not pass values after this date
-            todate=datetime.datetime(2020, 12, 31),
-            reverse=True,
+            todate=datetime.datetime(2022, 12, 31),
+#            reverse=True,
             dtformat=('%Y%m%d'),
-            datetime=2,
-            open=3,
-            high=4,
-            low=5,
-            close=6,
+            datetime=1,
+            open=2,
+            high=3,
+            low=4,
+            close=5,
             volume=-1,
             openinterest=-1
         )
@@ -141,7 +170,8 @@ if __name__ == '__main__':
 
     # Set our desired cash start
     cerebro.broker.setcash(100000.0)
-    cerebro.broker.setcommission(commission=0.05)
+#    cerebro.broker.setcommission(commission=0.005)
+    cerebro.addsizer(bt.sizers.FixedSize, stake=1000)
 
     # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
@@ -151,3 +181,5 @@ if __name__ == '__main__':
 
     # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    cerebro.plot(volume=False)
